d679ec9c488f57d8e6e18c5241e0ca199918f8e8
Exon Marcelo <exonmarcelo05@hotmail.com>
2024-04-28 12:33:43 -0500

    [FEAT] initial configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      spacing:{
+        '2': '2px',
+        '4': '4px',
+        '8': '8px',
+        '12': '12px',
+        '16': '16px',
+        '20': '20px',
+        '24': '24px',
+        '28': '28px',
+        '32': '32px',
+        '36': '36px',
+        '42': '42px',
+        '48': '48px',
+        '56': '56px',
+        '64': '64px',
+        '72': '72px',
+        '96': '96px',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
